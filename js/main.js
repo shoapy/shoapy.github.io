c36ece5f97dfb00776950c435adc347a0daf3485
shoapy <proyecto.shoapy@gmail.com>
2017-10-04 18:09:25 +0200

    More buttons...

--- js/main.js
+++ js/main.js
@@ -43,6 +43,11 @@ $(document).ready(function () {
         "height": (divImgHeight) + "px"
     });
 
+    $("#third-main-right").css({
+        "height": (divImgHeight * 0.75) + "px",
+        "width": (divImgHeight * 0.75) + "px"
+    });
+
     var overlayTop = $(".overlay").height() / 2 - $(".overlay").children().height() / 2;
     $(".overlay").children().css({
         "margin-top": (overlayTop) + "px"
@@ -86,4 +91,45 @@ $(document).ready(function () {
 
     secondMainContext.stroke();
 
+    var thirdMainCanvas = $("#third-main-right")[0];
+    thirdMainCanvas.width = thirdMainCanvas.scrollWidth;
+    thirdMainCanvas.height = thirdMainCanvas.scrollHeight;
+
+    var thirdMainContext = thirdMainCanvas.getContext("2d");
+
+    
+
 });
+
+function det (a11, a12, a13, a21, a22, a23, a31, a32, a33) {
+    res = a11 * a22 * a33 +
+          a12 * a23 * a31 +
+          a21 * a32 * a13 -
+          a13 * a22 * a31 -
+          a23 * a32 * a11 -
+          a12 * a21 * a33;
+
+    return res;
+}
+
+function parabola_fin (x0, y0, x1, y1) {
+    var a = det(y0, x0, 1, y1, x1, 1, 0, 1, 0) /
+            det(x0 * x0, x0, 1, x1 * x1, x1, 1, 2 * x1, 1, 0);
+    var b = det(x0 * x0, y0, 1, x1 * x1, y1, 1, 2 * x1, 0, 0) /
+            det(x0 * x0, x0, 1, x1 * x1, x1, 1, 2 * x1, 1, 0);
+    var c = det(x0 * x0, x0, y0, x1 * x1, x1, y1, 2 * x1, 1, 0) /
+            det(x0 * x0, x0, 1, x1 * x1, x1, 1, 2 * x1, 1, 0);
+
+    return [a, b, c];
+}
+
+function parabola_ini (x0, y0, x1, y1) {
+    var a = det(y0, x0, 1, y1, x1, 1, 0, 1, 0) /
+            det(x0 * x0, x0, 1, x1 * x1, x1, 1, 2 * x0, 1, 0);
+    var b = det(x0 * x0, y0, 1, x1 * x1, y1, 1, 2 * x0, 0, 0) /
+            det(x0 * x0, x0, 1, x1 * x1, x1, 1, 2 * x0, 1, 0);
+    var c = det(x0 * x0, x0, y0, x1 * x1, x1, y1, 2 * x0, 1, 0) /
+            det(x0 * x0, x0, 1, x1 * x1, x1, 1, 2 * x0, 1, 0);
+
+    return [a, b, c];
+}
