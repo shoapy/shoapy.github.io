aab07c52555012a909c658b55fb2b1517ca48a7d
shoapy <proyecto.shoapy@gmail.com>
2017-10-04 21:21:45 +0200

    Add flag to index.html

--- js/main.js
+++ js/main.js
@@ -44,13 +44,18 @@ $(document).ready(function () {
     });
 
     $("#third-main-right").css({
-        "height": (divImgHeight * 0.75) + "px",
+        "height": (divImgHeight * 0.5) + "px",
         "width": (divImgHeight * 0.75) + "px"
     });
 
-    var overlayTop = $(".overlay").height() / 2 - $(".overlay").children().height() / 2;
-    $(".overlay").children().css({
-        "margin-top": (overlayTop) + "px"
+    var overlayTop1 = $("#overlay1").height() / 2 - $("#overlay1").children().height() / 2;
+    $("#overlay1").children().css({
+        "margin-top": (overlayTop1) + "px"
+    });
+
+    var overlayTop2 = $("#overlay2").height() / 2 - $("#overlay2").children().height() / 2;
+    $("#overlay2").children().css({
+        "margin-top": (overlayTop2) + "px"
     });
 
     var secondMainCanvas = $("#second-main-left")[0];
@@ -97,7 +102,132 @@ $(document).ready(function () {
 
     var thirdMainContext = thirdMainCanvas.getContext("2d");
 
-    
+    var flagWidth = thirdMainCanvas.width;
+    var flagHeight = thirdMainCanvas.height;
+
+    thirdMainContext.beginPath();
+    thirdMainContext.fillStyle = "#89FFFA";
+
+    thirdMainContext.moveTo(0, flagHeight * 0.1);
+
+    var coefs = parabola_fin(0, flagHeight * 0.1, flagWidth * 0.4, 0);
+    for (var x = 0.0; x <= flagWidth * 0.4; x += 0.001) {
+        var y = coefs[0] * x * x + coefs[1] * x + coefs[2];
+        thirdMainContext.lineTo(x, y);
+    }
+
+    coefs = parabola_ini(flagWidth * 0.4, 0, flagWidth * 0.5, flagHeight * 0.05);
+    for (var x = flagWidth * 0.4; x <= flagWidth * 0.5; x += 0.001) {
+        var y = coefs[0] * x * x + coefs[1] * x + coefs[2];
+        thirdMainContext.lineTo(x, y);
+    }
+
+    coefs = parabola_fin(flagWidth * 0.5, flagHeight * 0.05, flagWidth * 0.6, flagHeight * 0.1);
+    for (var x = flagWidth * 0.5; x <= flagWidth * 0.6; x += 0.001) {
+        var y = coefs[0] * x * x + coefs[1] * x + coefs[2];
+        thirdMainContext.lineTo(x, y);
+    }
+
+    coefs = parabola_ini(flagWidth * 0.6, flagHeight * 0.1, flagWidth, 0);
+    for (var x = flagWidth * 0.6; x <= flagWidth; x += 0.001) {
+        var y = coefs[0] * x * x + coefs[1] * x + coefs[2];
+        thirdMainContext.lineTo(x, y);
+    }
+
+    thirdMainContext.lineTo(flagWidth, flagHeight * 0.9);
+
+    coefs = parabola_ini(flagWidth * 0.6, flagHeight, flagWidth, flagHeight * 0.9);
+    for (var x = flagWidth; x >= flagWidth * 0.6; x -= 0.001) {
+        var y = coefs[0] * x * x + coefs[1] * x + coefs[2];
+        thirdMainContext.lineTo(x, y);
+    }
+
+    coefs = parabola_fin(flagWidth * 0.5, flagHeight * 0.95, flagWidth * 0.6, flagHeight);
+    for (var x = flagWidth * 0.6; x >= flagWidth * 0.5; x -= 0.001) {
+        var y = coefs[0] * x * x + coefs[1] * x + coefs[2];
+        thirdMainContext.lineTo(x, y);
+    }
+
+    coefs = parabola_ini(flagWidth * 0.4, flagHeight * 0.9, flagWidth * 0.5, flagHeight * 0.95);
+    for (var x = flagWidth * 0.5; x >= flagWidth * 0.4; x -= 0.001) {
+        var y = coefs[0] * x * x + coefs[1] * x + coefs[2];
+        thirdMainContext.lineTo(x, y);
+    }
+
+    coefs = parabola_fin(0, flagHeight, flagWidth * 0.4, flagHeight * 0.9);
+    for (var x = flagWidth * 0.4; x >= 0.0; x -= 0.001) {
+        var y = coefs[0] * x * x + coefs[1] * x + coefs[2];
+        thirdMainContext.lineTo(x, y);
+    }
+
+    thirdMainContext.lineTo(0, flagHeight * 0.1);
+
+    thirdMainContext.fill();
+
+    var scaleX = 0.9;
+    var scaleY = 0.85;
+    var x_add = flagWidth * (1 - scaleX) / 2;
+    var y_add = flagHeight * (1 - scaleY) / 2;
+
+    thirdMainContext.beginPath();
+    thirdMainContext.strokeStyle = "white";
+    thirdMainContext.lineWidth = "5";
+
+    thirdMainContext.moveTo(0 + x_add, flagHeight * 0.1 * scaleY + y_add);
+
+    var coefs = parabola_fin(0, flagHeight * 0.1, flagWidth * 0.4, 0);
+    for (var x = 0.0; x <= flagWidth * 0.4; x += 0.001) {
+        var y = coefs[0] * x * x + coefs[1] * x + coefs[2];
+        thirdMainContext.lineTo(x * scaleX + x_add, y * scaleY + y_add);
+    }
+
+    coefs = parabola_ini(flagWidth * 0.4, 0, flagWidth * 0.5, flagHeight * 0.05);
+    for (var x = flagWidth * 0.4; x <= flagWidth * 0.5; x += 0.001) {
+        var y = coefs[0] * x * x + coefs[1] * x + coefs[2];
+        thirdMainContext.lineTo(x * scaleX + x_add, y * scaleY + y_add);
+    }
+
+    coefs = parabola_fin(flagWidth * 0.5, flagHeight * 0.05, flagWidth * 0.6, flagHeight * 0.1);
+    for (var x = flagWidth * 0.5; x <= flagWidth * 0.6; x += 0.001) {
+        var y = coefs[0] * x * x + coefs[1] * x + coefs[2];
+        thirdMainContext.lineTo(x * scaleX + x_add, y * scaleY + y_add);
+    }
+
+    coefs = parabola_ini(flagWidth * 0.6, flagHeight * 0.1, flagWidth, 0);
+    for (var x = flagWidth * 0.6; x <= flagWidth; x += 0.001) {
+        var y = coefs[0] * x * x + coefs[1] * x + coefs[2];
+        thirdMainContext.lineTo(x * scaleX + x_add, y * scaleY + y_add);
+    }
+
+    thirdMainContext.lineTo(flagWidth * scaleX + x_add, flagHeight * 0.9 * scaleY + y_add);
+
+    coefs = parabola_ini(flagWidth * 0.6, flagHeight, flagWidth, flagHeight * 0.9);
+    for (var x = flagWidth; x >= flagWidth * 0.6; x -= 0.001) {
+        var y = coefs[0] * x * x + coefs[1] * x + coefs[2];
+        thirdMainContext.lineTo(x * scaleX + x_add, y * scaleY + y_add);
+    }
+
+    coefs = parabola_fin(flagWidth * 0.5, flagHeight * 0.95, flagWidth * 0.6, flagHeight);
+    for (var x = flagWidth * 0.6; x >= flagWidth * 0.5; x -= 0.001) {
+        var y = coefs[0] * x * x + coefs[1] * x + coefs[2];
+        thirdMainContext.lineTo(x * scaleX + x_add, y * scaleY + y_add);
+    }
+
+    coefs = parabola_ini(flagWidth * 0.4, flagHeight * 0.9, flagWidth * 0.5, flagHeight * 0.95);
+    for (var x = flagWidth * 0.5; x >= flagWidth * 0.4; x -= 0.001) {
+        var y = coefs[0] * x * x + coefs[1] * x + coefs[2];
+        thirdMainContext.lineTo(x * scaleX + x_add, y * scaleY + y_add);
+    }
+
+    coefs = parabola_fin(0, flagHeight, flagWidth * 0.4, flagHeight * 0.9);
+    for (var x = flagWidth * 0.4; x >= 0.0; x -= 0.001) {
+        var y = coefs[0] * x * x + coefs[1] * x + coefs[2];
+        thirdMainContext.lineTo(x * scaleX + x_add, y * scaleY + y_add);
+    }
+
+    thirdMainContext.lineTo(0 + x_add, flagHeight * 0.1 * scaleY + y_add);
+
+    thirdMainContext.stroke();
 
 });
 
