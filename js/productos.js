10e7e88c86671cf9203ce082affb3dff5264c370
shoapy <proyecto.shoapy@gmail.com>
2017-10-11 13:58:16 +0200

    Add second product description

--- js/productos.js
+++ js/productos.js
@@ -8,22 +8,35 @@ $(document).ready(function () {
     $("#first").css({
         "height": (productContainerHeight) + "px"
     });
+    $("#second").css({
+        "height": (productContainerHeight) + "px"
+    });
+
+    $(".product-pic").css({
+        "height": (productContainerHeight * 0.9) + "px",
+        "width": (productContainerHeight * 0.9) + "px",
+    });
 
     var topInfoPos1 = productContainerHeight / 2 - $("#product-info-1").height() / 2;
     $("#product-info-1").css({
         "margin-top": (topInfoPos1) + "px",
-        "margin-left": "50px"
-    });
-
-    $("#product-pic-1").css({
-        "height": (productContainerHeight * 0.9) + "px",
-        "width": (productContainerHeight * 0.9) + "px",
+        "padding-left": "50px"
     });
 
     var topPicPos1 = productContainerHeight / 2 - $("#product-pic-1").height() / 2;
     $("#product-pic-1").css({
         "margin-top": (topPicPos1) + "px",
-        "margin-left": "50px"
+    });
+
+    var topInfoPos2 = productContainerHeight / 2 - $("#product-info-2").height() / 2;
+    $("#product-info-2").css({
+        "margin-top": (topInfoPos2) + "px",
+        "padding-left": "50px"
+    });
+
+    var topPicPos2 = productContainerHeight / 2 - $("#product-pic-2").height() / 2;
+    $("#product-pic-2").css({
+        "margin-top": (topPicPos2) + "px"
     });
 
 });
