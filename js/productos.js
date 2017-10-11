8784ae0f9a031d6eea1bcad5c16a6635adc6d79c
shoapy <proyecto.shoapy@gmail.com>
2017-10-11 12:07:27 +0200

    Add first product

--- js/productos.js
+++ js/productos.js
@@ -2,13 +2,28 @@ $(document).ready(function () {
 
     var windowWidth = $(window).width();
     var windowHeight = $(window).height();
-    var headerHeight = $("#main").offset().top;
+    var headerHeight = $("#first").offset().top;
 
     var productContainerHeight = windowHeight - headerHeight;
-    $(".product-container").css({
+    $("#first").css({
         "height": (productContainerHeight) + "px"
     });
 
-    alert('scnidufcn');
+    var topInfoPos1 = productContainerHeight / 2 - $("#product-info-1").height() / 2;
+    $("#product-info-1").css({
+        "margin-top": (topInfoPos1) + "px",
+        "margin-left": "50px"
+    });
+
+    $("#product-pic-1").css({
+        "height": (productContainerHeight * 0.9) + "px",
+        "width": (productContainerHeight * 0.9) + "px",
+    });
+
+    var topPicPos1 = productContainerHeight / 2 - $("#product-pic-1").height() / 2;
+    $("#product-pic-1").css({
+        "margin-top": (topPicPos1) + "px",
+        "margin-left": "50px"
+    });
 
 });
